81297b4ade28ad4ffd7a1403f3286371b769ae56
qingshuiqianhe <qingshuiqian12@gmail.com>
2018-11-02 15:12:14 +0800

    修改test2

--- 无重复字符的最长字符串.py
+++ 无重复字符的最长字符串.py
@@ -5,42 +5,55 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        count = 1
-        list1 = list(s)
-        list2 = []
-        for i in range(1, len(list1)):
-            if list1[i - 1] != list1[i] and list1[i] not in list2:
-                if i == 1:
-                    list2.append(list1[i - 1])
-                list2.append(list1[i])
-                count += 1
-            if list1[i] in list2:
 
-                if i == len(list1) - 1:
-                    return count
+        max_len = 0
+        if s is None or len(s) == 0:
+            return max_len
+        str_dict = {}
 
+        one_max = 0
+        start = 0
+        for i in range(len(s)):
+            if s[i] in str_dict and str_dict[s[i]] >= start:
+                start = str_dict[s[i]] + 1
+            one_max = i - start + 1
+            str_dict[s[i]] = i
+            max_len = max(max_len, one_max)
+        return max_len
 
-    def test2(self, s):
-        list1 = list(s)
-        count = 1
-        list2 = []
-        mm = ''
-        for i in range(1, len(list1)):
-            if list1[i - 1] != list1[i] and list1[i] not in list2:
-                if i == 1:
-                    list2.append(list1[i - 1])
-                    mm += list1[i-1]
-                list2.append(list1[i])
-                count += 1
-                mm += list1[i]
-            if list1[i] in list2:
-                if i == len(list1) - 1:
-                    return mm, count
+    def sss(self, s):
+        if len(s) == 1:
+            return 1
+        if s == '':
+            return 0
+        alllist = []
+        a = ''
+        for i in range(len(s)):
+            if s[i] not in a:
+                a += s[i]
+                if len(a) == len(s):
+                    return len(s)
+            else:
+                alllist.append(a)
+                for j in range(len(a)):
+                    if a[j] == s[i]:
+                        print a[j + 1:], s[i]
+                        a = a[j + 1:] + s[i]
+                        break
+                continue
+        alllist.append(a)
+        for i in range(len(alllist)):
+            alllist[i] = len(alllist[i])
+
+        lengthsubstring = max(alllist)
+        return lengthsubstring
+
 
 
 if __name__ == "__main__":
-    s = 'asdfghjg'
+    s = 'aaabaaaaaaaaa'
     ss = Solution()
-    print ss.lengthOfLongestSubstring(s)
-    print ss.test2(s)
+    # print ss.lengthOfLongestSubstring(s)
+    print ss.sss(s)
+
 
